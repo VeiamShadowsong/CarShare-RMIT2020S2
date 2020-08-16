7d25b93f75a9d60b63752add5afd112da7a82bd0
Edgar Tang <edgar@veiam.net>
2020-08-16 18:59:49 +1000

    feat(admin): build admin login and logout

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return redirect('admin/dashboard');
+    });
+
+    Route::get('/login', function () {
+        return view('admin.login');
+    });
+    Route::post('/login', 'AdminController@adminLogin');
+
+    Route::group(['middleware' => ['web','admin.logged.in']], function () {
+        Route::get('/logout', 'AdminController@adminLogout');
+        Route::get('/dashboard', function () {
+            return view('admin.dashboard');
+        });
+    });
+});
