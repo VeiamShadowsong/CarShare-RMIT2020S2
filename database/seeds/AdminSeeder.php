06dd10a0a43c7560c52bfc33c4cbc91547fac0b8
Edgar Tang <edgar@veiam.net>
2020-08-16 18:57:36 +1000

    feat(admin): build admin migrate file and seeder

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admins = [
+            [
+                'username' => 'hdplz',
+                'password' => Hash::make('hdplz'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
+
+        // Create Administrators
+        DB::statement("SET foreign_key_checks=0");
+        DB::table('admins')->truncate();
+        DB::statement("SET foreign_key_checks=1");
+        DB::table('admins')->insert($admins);
+    }
+}
