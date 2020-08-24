fd16fc15c3fb059fc26e219a5b169aef7f36593f
Edgar Tang <edgar@veiam.net>
2020-08-24 16:52:45 +1000

    fix<layout>: fix layout text error

--- resources/views/layout/adminLayout.blade.php
+++ resources/views/layout/adminLayout.blade.php
@@ -235,7 +235,7 @@
 				<div id="m_ver_menu" class="m-aside-menu  m-aside-menu--skin-dark m-aside-menu--submenu-skin-dark " data-menu-vertical="true" m-menu-scrollable="1" m-menu-dropdown-timeout="500">
 					<ul class="m-menu__nav  m-menu__nav--dropdown-submenu-arrow ">
 						<li class="m-menu__section m-menu__section--first">
-							<h4 class="m-menu__section-text">STUDENTS</h4>
+							<h4 class="m-menu__section-text">USERS</h4>
 							<i class="m-menu__section-icon flaticon-more-v2"></i>
 						</li>
 						<li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="{{url('/admin/dashboard')}}" class="m-menu__link "><i class="m-menu__link-icon flaticon-dashboard"></i><span class="m-menu__link-text">Users Dashboard</span></a></li>
