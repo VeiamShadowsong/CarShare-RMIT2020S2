e873f8a7a4d83349bed9f0ab7afcc1be5996ef7b
Edgar Tang <edgar@veiam.net>
2020-09-05 20:49:15 +1000

    feat(car): update car position on the map

--- resources/views/user/cars.blade.php
+++ resources/views/user/cars.blade.php
@@ -31,49 +31,56 @@
     <script>
         "use strict";
 
-        // This example displays a marker at the center of Australia.
-        // When the user clicks the marker, an info window opens.
         function initMap() {
-            const uluru = {
-                lat: -25.363,
-                lng: 131.044
-            };
+            const qv = new google.maps.LatLng(-37.8106277, 144.9634516);
             const map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 4,
-                center: uluru
+                zoom: 15,
+                center: qv
             });
-            const contentString =
-                '<div id="content">' +
-                '<div id="siteNotice">' +
-                "</div>" +
-                '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-                '<div id="bodyContent">' +
-                "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-                "sandstone rock formation in the southern part of the " +
-                "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-                "south west of the nearest large town, Alice Springs; 450&#160;km " +
-                "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-                "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-                "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-                "Aboriginal people of the area. It has many springs, waterholes, " +
-                "rock caves and ancient paintings. Uluru is listed as a World " +
-                "Heritage Site.</p>" +
-                '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-                "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-                "(last visited June 22, 2009).</p>" +
-                "</div>" +
-                "</div>";
-            const infowindow = new google.maps.InfoWindow({
-                content: contentString
+            const userInfo = new google.maps.InfoWindow({
+                content: "Your position is QV"
             });
-            const marker = new google.maps.Marker({
-                position: uluru,
+            const userMarker = new google.maps.Marker({
+                position: qv,
                 map,
-                title: "Uluru (Ayers Rock)"
+                title: "Your position"
             });
-            marker.addListener("click", () => {
-                infowindow.open(map, marker);
+            userMarker.addListener("click", () => {
+                userInfo.open(map, userMarker);
             });
+
+            var cars = [];
+            @foreach($cars as $car)
+            cars.push([
+                new google.maps.Marker({
+                    position: new google.maps.LatLng({{$car->lat}}, {{$car->lng}}),
+                    map,
+                    title: "{{$car->number}}"
+                }),
+                new google.maps.InfoWindow({
+                    content:
+                        '<div id="content">' +
+                            '<div id="siteNotice">' +
+                            "</div>" +
+                                '<h1 id="firstHeading" class="firstHeading">{{$car->number}}</h1>' +
+                                '<div id="bodyContent">' +
+                                "<p><b>Make: </b>{{$car->make->name}}</p>" +
+                                "<p><b>Type: </b>{{$car->type}}</p>" +
+                                "<p><b>Color: </b>{{$car->color}}</p>" +
+                                "<p><b>Status: </b>{{$car->status}}</p>" +
+                                "<p><b>Owner: </b>{{$car->user->first_name}} {{$car->user->last_name}}</p>" +
+                                `<a href="{{url('/cars/order')}}/{{$car->id}}" class="btn btn-primary m-btn m-btn--pill m-btn--custom m-btn--icon m-btn--air">Order</a>` +
+                            "</div>" +
+                        "</div>"
+                })
+            ]);
+            @endforeach
+
+            for(let i = 0; i < cars.length; i++) {
+                cars[i][0].addListener("click", () => {
+                    cars[i][1].open(map, cars[i][0]);
+                });
+            }
         }
 
         initMap()
