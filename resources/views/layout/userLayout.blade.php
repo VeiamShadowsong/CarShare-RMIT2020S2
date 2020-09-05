d7d75eb7bc6b3474f35c60848a506990d5879f57
Edgar Tang <edgar@veiam.net>
2020-09-05 21:37:43 +1000

    style(footer): update powered by detail

--- resources/views/layout/userLayout.blade.php
+++ resources/views/layout/userLayout.blade.php
@@ -251,7 +251,7 @@
 					<div class="m-stack m-stack--flex-tablet-and-mobile m-stack--ver m-stack--desktop">
 						<div class="m-stack__item m-stack__item--left m-stack__item--middle m-stack__item--last">
 							<span class="m-footer__copyright">
-								2020 &copy; Powerd by <a href="https://zetadigital.com.au" class="m-link">Zeta Digital Pty Ltd</a>
+								2020 &copy; Powerd by <a href="https://github.com/HundredEyesMonk/ProgrammingProject2020.git" class="m-link">Group HDPLZ</a>
 							</span>
 						</div>
 					</div>
