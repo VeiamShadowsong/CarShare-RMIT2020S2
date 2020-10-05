a456ebf43375c62e3ab42d882fb337b87fcef6d1
Edgar Tang <edgar@veiam.net>
2020-10-05 12:58:58 +1100

    style(layout): fix arrow icon

--- resources/views/layout/userLayout.blade.php
+++ resources/views/layout/userLayout.blade.php
@@ -260,7 +260,7 @@
 						</li>
 						<li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="{{url('/dashboard')}}" class="m-menu__link "><i class="m-menu__link-icon flaticon-dashboard"></i><span class="m-menu__link-text">Users Dashboard</span></a></li>
 						<li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover"><a href="javascript:;" class="m-menu__link m-menu__toggle"><i class="m-menu__link-icon fa fa-car"></i><span class="m-menu__link-text">Cars</span><i
-										class="m-menu__ver-arrow la la-car"></i></a>
+										class="m-menu__ver-arrow la la-angle-right"></i></a>
 							<div class="m-menu__submenu "><span class="m-menu__arrow"></span>
 								<ul class="m-menu__subnav">
 									<li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="{{url('/cars')}}" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span class="m-menu__link-text">Cars</span></a></li>
