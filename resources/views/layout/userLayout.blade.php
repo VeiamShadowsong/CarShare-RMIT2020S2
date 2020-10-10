0e784031dd13afcc215441833830d450c7510aba
Edgar Tang <edgar@veiam.net>
2020-10-11 09:06:57 +1100

    feat(car) update admin and user car list

--- resources/views/layout/userLayout.blade.php
+++ resources/views/layout/userLayout.blade.php
@@ -135,8 +135,8 @@
 															<li class="m-menu__item">
 																<ul class="m-menu__inner">
 																	<br/>
-																	<li class="m-menu__item " m-menu-link-redirect="1" aria-haspopup="true"><a href="{{url('/cars')}}" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--line"><span></span></i><span class="m-menu__link-text">Cars</span></a></li>
-																	<li class="m-menu__item " m-menu-link-redirect="1" aria-haspopup="true"><a href="{{url('/cars/list')}}" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--line"><span></span></i><span class="m-menu__link-text">List</span></a></li>
+																	<li class="m-menu__item " m-menu-link-redirect="1" aria-haspopup="true"><a href="{{url('/cars')}}" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--line"><span></span></i><span class="m-menu__link-text">Nearby Cars</span></a></li>
+																	<li class="m-menu__item " m-menu-link-redirect="1" aria-haspopup="true"><a href="{{url('/cars/list')}}" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--line"><span></span></i><span class="m-menu__link-text">My Cars</span></a></li>
 																</ul>
 															</li>
 														</ul>
@@ -263,8 +263,8 @@
 										class="m-menu__ver-arrow la la-angle-right"></i></a>
 							<div class="m-menu__submenu "><span class="m-menu__arrow"></span>
 								<ul class="m-menu__subnav">
-									<li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="{{url('/cars')}}" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span class="m-menu__link-text">Cars</span></a></li>
-									<li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="{{url('/cars/list')}}" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span class="m-menu__link-text">List</span></a></li>
+									<li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="{{url('/cars')}}" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span class="m-menu__link-text">Nearby Cars</span></a></li>
+									<li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="{{url('/cars/list')}}" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span class="m-menu__link-text">My Cars</span></a></li>
 								</ul>
 							</div>
 						</li>
